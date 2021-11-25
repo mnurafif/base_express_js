cbc5707aff37b4fe5d222a7715292a1fecfb3d1e
mnurafif <94831235+mnurafif@users.noreply.github.com>
2021-11-25 14:17:59 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,63 +15,46 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         global_msg_id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /show_url`
+        click /jenis makhluk hidup`
     );
 });
 
-bot.onText(/\/show_url/, (msg) => {
+bot.onText(/\/jenis makhluk hidup/, (msg) => {
     global_msg_id = msg.chat.id;
     bot.sendMessage(
         global_msg_id,
         `
-            https://esp-telebot.herokuapp.com/api/sensor/123/65/78 \n
-            https://esp-telebot.herokuapp.com/api/test/cobacoba
+            click/manusia \n
+            click/hewan \n
+            click/tumbuhan
+        `
+    );
+});
+bot.onText(/\/manusia/, (msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+            click/Pria \n
+            click/Wanita
         `
     );
 });
 
+bot.onText(/\/Pria/, (msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
+  https://www.google.com/search?q=pria&sxsrf=AOaemvKd7dYezj1iAnOM_ALpYYmbfrjwQw:1637824622428&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj1vMuW_LL0AhUsILcAHY0mAE0Q_AUoAXoECAIQAw&biw=1280&bih=569&dpr=1.5
+        `
+    );
+});
 bot.on('message', (msg) => {
   console.log(msg);
 });
 
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.json({
-    "status": 202,
-    "messgae": "Success"
-  });
-});
-
-// https://esp-telebot.herokuapp.com/api/sensor/123/65/78
-router.get('/sensor/:sensor1/:sensor2/:sensor3', (req, res, next) => {
-  try {
-      bot.sendMessage(
-            global_msg_id, //msg.id
-            `Pembacaan Sensor:: ${req.params.sensor1}, ${req.params.sensor2}, ${req.params.sensor3}`
-     );
-      res.json({
-        "status": 202,
-        "messgae": "Success",
-        "data": {
-          "sensor_1": req.params.sensor1,
-          "sensor_2": req.params.sensor2,
-          "sensor_3": req.params.sensor3
-        }
-      });
-  } catch (err) {
-      next(err);
-  }
-});
-
-// https://esp-telebot.herokuapp.com/api/test/cobacoba
-router.get('/test/:key', function(req, res, next){
-    bot.sendMessage(
-            global_msg_id, //msg.id
-            `${req.params.key}`
-    );
-    res.json(req.params.key);
-});
 
 
 module.exports = router;
