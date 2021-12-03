40cc1d708b9e70cb7bd95a3636320cee40ad9028
mnurafif <94831235+mnurafif@users.noreply.github.com>
2021-12-03 18:54:47 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ bot.onText(/\/show_url/, (msg) => {
     bot.sendMessage(
         global_msg_id,
         `
-            /nhttps://esp-telebot.herokuapp.com/api/sensor/123/65/78
+            https://esp-telebot.herokuapp.com/api/sensor/123/65/78 \nhttps://esp-telebot.herokuapp.com/api/test/cobacoba
         `
     );
 });
